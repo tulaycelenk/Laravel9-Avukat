37c4275431dbf241a89417ac779935c0fdabcf9b
tulaycelenk <tulaycelnk0@gmail.com>
2022-05-14 00:06:41 +0300

    delete function executed

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class CategoryController extends Controller
 {
@@ -105,8 +106,11 @@ class CategoryController extends Controller
      * @param  \App\Models\Category  $category
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Category $category)
+    public function destroy(Category $category,$id)
     {
-        //
+        $data=Category::find($id);
+        Storage::delete($data->image);
+        $data->delete();
+        return redirect('admin/category');
     }
 }
