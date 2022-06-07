c602bbc917fdd2f53c0d508cee02d6f70a962a45
tulaycelenk <tulaycelnk0@gmail.com>
2022-06-07 06:42:36 +0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Admin\CategoryController as adminCategoryController;
 use App\Http\Controllers\Admin\ServiceController as adminServiceController;
 use App\Http\Controllers\Admin\ImageController as adminImageController;
 use App\Http\Controllers\Admin\FaqController as adminFAQController;
+use App\Http\Controllers\Admin\AppointmentController as adminAppointmentController;
 
 use Illuminate\Support\Facades\Route;
 
@@ -35,17 +36,34 @@ Route::get('/portfolio.html', [HomeController::class,'portfolio'])->name('portfo
 Route::get('/test', [HomeController::class,'test'])->name('test');
 Route::get('/prm/{id}/{number}', [HomeController::class,'param'])->name('param');
 Route::post('/faq', [HomeController::class,'faq'])->name('faq');
+Route::get('/loginuser', [HomeController::class,'loginuser'])->name('loginuser');
+Route::get('/logoutuser', [HomeController::class,'logoutuser'])->name('logoutuser');
+Route::get('/myAppointments', [HomeController::class,'myAppointments'])->name('myAppointments');
+Route::get('/cancelAppointment/{id}', [adminAppointmentController::class,'cancel'])->name('cancelAppointment');
+
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::post('/requestForAppointment', [HomeController::class,'requestForAppointment'])->name('requestForAppointment');
+
+//service list page
+Route::get('/category/{id}', [HomeController::class,'categoryListPage'])->name('categoryListPage');
+
+//service detail page
+Route::get('/service/{id}', [HomeController::class,'serviceDetailPage'])->name('serviceDetailPage');
 
 
 // ADMIN PANEL ROUTES
 Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('/',[adminHomeController::class,'index'])->name('index');
+    Route::get('/setting',[adminHomeController::class,'setting'])->name('setting');
+    Route::get('/setting',[adminHomeController::class,'settingUpdate'])->name('setting.update');
     //ADMIN CATEGORY ROUTES
     Route::prefix('/category')->name('category.')->controller(adminCategoryController::class)->group(function () {
         Route::get('/','index')->name('index');
+        Route::get('/create','create')->name('create');
         Route::post('/store','store')->name('store');
         Route::get('/edit/{id}','edit')->name('edit');
         Route::post('/update/{id}','update')->name('update');
@@ -95,6 +113,14 @@ Route::prefix('admin')->name('admin.')->group(function () {
         Route::post('/update/{id}','update')->name('update');
         Route::get('/destroy/{id}','destroy')->name('destroy');
         Route::get('/show/{id}','show')->name('show');
+
+    });
+
+    //ADMIN APPOINTMENT ROUTES
+    Route::prefix('/appointment')->name('appointment.')->controller(adminAppointmentController::class)->group(function () {
+        Route::get('/','index')->name('index');
+        Route::get('/approve_appointment/{id}', 'approve')->name('approve_appointment');
+        Route::get('/decline_appointment/{id}', 'decline')->name('decline_appointment');
 
     });
 });
